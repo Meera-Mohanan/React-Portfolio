1e50581a04c1760088420c0cc3f36f5f3b7cfecd
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-14 01:01:38 +0800

    resume added

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,38 +1,42 @@
 import React from 'react';
-// import resumePdf from '../../assets/images/';
-
+import resumePdf from '../../assets/images/resume-Meera.pdf';
 const Resume = () => (
-    <div className="resumeContainer">
-        <h2>Resume</h2>
-        <div>
-            {/* <p>Download My <a className="resumeLink" href={resumePdf} download="resume"> Resume </a></p> */}
-        </div>
-        <div className="resumeHead">Front-End Proficiencies:</div>
-        <ul className="resume">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>Jquery</li>
-            <li>Responsive Design</li>
-            <li>React</li>
-            <li>Bootstrap</li>
-        </ul>
-        <div className="resumeHead">Back-End proficiencies: </div>
-        <ul className="resume">
-            <li>Nodejs</li>
-            <li>Expressjs</li>
-            <li>MySql, Sequelize</li>
-            <li>MongoDB, Mongoose</li>            
-        </ul>
-        <div className="resumeHead">Full-Stack proficiencies: </div>
-        <ul className="resume">
-            <li>MVC</li>
-            <li>PWA</li>
-            <li>REST API</li>
-            <li>MERN Stack</li>
-            <li>GraphQL</li>
-        </ul>
-    </div>  
+  <div className="resumeContainer">
+    <h2>Resume</h2>
+    <div>
+      <p>
+        Download My{' '}
+        <a className="resumeLink" href={resumePdf} download="resume">
+          {' '}
+          Resume{' '}
+        </a>
+      </p>
+    </div>
+    <div className="resumeHead">Front-End Proficiencies:</div>
+    <ul className="resume">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li>Jquery</li>
+      <li>Responsive Design</li>
+      <li>React</li>
+      <li>Bootstrap</li>
+    </ul>
+    <div className="resumeHead">Back-End proficiencies: </div>
+    <ul className="resume">
+      <li>Nodejs</li>
+      <li>Expressjs</li>
+      <li>MySql, Sequelize</li>
+      <li>MongoDB, Mongoose</li>
+    </ul>
+    <div className="resumeHead">Full-Stack proficiencies: </div>
+    <ul className="resume">
+      <li>MVC</li>
+      <li>PWA</li>
+      <li>REST API</li>
+      <li>MERN Stack</li>
+      <li>GraphQL</li>
+    </ul>
+  </div>
 );
-
 export default Resume;
